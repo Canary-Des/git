9d38ec9b489244e36695b6778a526d15c70dd9ab
Canary-Des <74391961+Canary-Des@users.noreply.github.com>
2022-09-07 15:30:37 +1000

    Add files via upload

---
+++ DETECTION-CPRAM-Company-L.ps1
@@ -0,0 +1,24 @@
+
+
+$NetworkPath = "L:\"
+
+try {
+
+    If (Test-Path -Path $NetworkPath -ErrorAction Stop) {
+
+        Write-Host "Network path is exists." -ForegroundColor Green
+        exit 0
+
+    } else {
+
+        Write-Host "Network path is not mapped." -ForegroundColor Yellow
+        exit 1
+    }
+
+} catch {
+
+    $ErrMsg = $_.Exception.Message
+    Write-Host $ErrMsg
+    exit 1
+    
+}
